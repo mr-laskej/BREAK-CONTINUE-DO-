1cd01439b519a6a16a92b5d3c4d17169e69f1a50
matyas.lasky.020 <matyas.lasky.020@PC2017-09>
2022-02-07 13:25:39 +0100

    Přidejte soubory projektu.

---
+++ FORTNITE.cpp
@@ -0,0 +1,31 @@
+﻿//ukázka break, continue a switch 
+//dn
+#include <stdio.h>
+
+int main(void)
+{
+	printf("BREAK\n");
+	for (int j = 1; j <= 3; j++) {
+		printf("j: %d\n", j);
+		for (int i = 0; i <= 100; i++) {
+			printf("%d ", i);
+			if (i > 10) break;
+		}
+		printf("\n");
+	}
+	printf("CONTINUE \n");
+	int i = 0;
+	while (i < 100) {
+		i++;
+		if (i < 10) continue;
+			printf("%d ", i);
+	}
+	printf("\n");
+	printf("DO\n");
+	printf("smycka do: \n");
+	i = 0;
+	do {
+		printf("%d ", i);
+		i++;
+	} while (i < 10);
+}
